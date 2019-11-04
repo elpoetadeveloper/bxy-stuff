cedc902e2cc1e89f0831c3c2534f15f049fb0048
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-11-04 20:20:12 -0300

    file tree

--- app.js
+++ app.js
@@ -4,3 +4,28 @@ const template = '<h2>New Content</h2>';
 btnPage.addEventListener('click', e => {
   main.innerHTML = template;
 });
+
+$(function () {
+  var jsondata = [
+    { id: "ajson1", parent: "#", text: "Simple root node" },
+    { id: "ajson2", parent: "#", text: "Root node 2" },
+    { id: "ajson3", parent: "ajson2", text: "Child 1" },
+    { id: "ajson4", parent: "ajson2", text: "Child 2" }
+  ];
+
+  createJSTree(jsondata);
+});
+
+function createJSTree(jsondata) {
+  $("#mytree").jstree({
+    core: {
+      data: jsondata,
+      themes: { stripes: true, responsive: true, variant: "large" }
+    },
+    plugins: ["search", "themes", "json_data", "ui", "dnd"],
+    search: {
+      case_sensitive: false,
+      show_only_matches: true
+    }
+  });
+}
