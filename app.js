90fa793e7b42a59a0ff8672d97b6aad4d584291d
Leonardo Tosetto <leonardo.a.tosetto@gmail.com>
2019-11-04 21:41:22 -0300

    file tree

--- app.js
+++ app.js
@@ -10,7 +10,11 @@ $(function () {
     { id: "ajson1", parent: "#", text: "Simple root node" },
     { id: "ajson2", parent: "#", text: "Root node 2" },
     { id: "ajson3", parent: "ajson2", text: "Child 1" },
-    { id: "ajson4", parent: "ajson2", text: "Child 2" }
+    { id: "ajson4", parent: "ajson2", text: "Child 2" },
+    { id: "ajson5", parent: "#", text: "Root node 3" },
+    { id: "ajson6", parent: "ajson5", text: "Child 1" },
+    { id: "ajson7", parent: "ajson5", text: "Child 2" },
+    { id: "ajson8", parent: "ajson7", text: "Child 1" }
   ];
 
   createJSTree(jsondata);
@@ -20,9 +24,17 @@ function createJSTree(jsondata) {
   $("#mytree").jstree({
     core: {
       data: jsondata,
-      themes: { stripes: true, responsive: true, variant: "large" }
+      themes: { stripes: true, responsive: true, variant: "large", "icons": true, "dots": true }
     },
-    plugins: ["search", "themes", "json_data", "ui", "dnd"],
+    'types': {
+      'root': { 'icon': 'folder' },
+      'leaf': { 'icon': 'folder' },
+      'project': { 'icon': 'folder' },
+      'folder': { 'icon': 'folder' },
+      'default': { 'icon': 'folder' },
+      'file': { 'valid_children': [], 'icon': 'file' }
+    },
+    plugins: ["search", "themes", "json_data", "ui", "dnd", "types"],
     search: {
       case_sensitive: false,
       show_only_matches: true
